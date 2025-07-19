e43a6aab0320f83b04e15b4c4213099e02697d4c
AbdElrhman Ahmed <abdelrhman.rezk7395@gmail.com>
2025-07-19 18:37:11 +0300

    Add Supabase integration and update plugins
    Integrated Supabase with Flutter by adding supabase_flutter to dependencies and initializing it in main.dart. Updated platform plugin registrants and configuration files to support new plugins (app_links, url_launcher, shared_preferences, etc.) across Linux, macOS, and Windows. Added empty login_screen.dart and auth_gate.dart for authentication flow scaffolding.

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:repx/core/theme/app_theme.dart';
 import 'package:repx/views/initial_screen.dart';
 import 'package:repx/views/splash/splash_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  //Supabase connection
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://onbmyrlmcowiyyedntuw.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uYm15cmxtY293aXl5ZWRudHV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5NDk2OTIsImV4cCI6MjA2NzUyNTY5Mn0.1GHsU3ZgV0zqPAl-drskh8_rnLJAM3SHogqgkkAEP6M',
+  );
   runApp(const Repx());
 }
 
